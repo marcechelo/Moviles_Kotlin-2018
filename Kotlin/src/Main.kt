a7d8ac1e62d875cfc77078f4161726b074bc8d97
Marcelo Nieto <marcechelo25@hotmail.com>
2018-04-18 10:12:35 -0500

    when y variables

--- Kotlin/src/Main.kt
+++ Kotlin/src/Main.kt
@@ -5,4 +5,30 @@ fun sumarDosNumeros(numeroUno: Int, numeroDos: Int): Int{
 fun main(args: Array<String>){
     println("Hola Mundo") //system.out
     println(sumarDosNumeros(1,2))
+
+    var nombre: String? = "Marcelo"
+    var edadActual = 22
+    var casado = false
+    //duck typing
+    casado = true
+    nombre = null
+
+    val superHeroe = if (casado){
+        "SuperMandarina"
+    }else{
+        "Superman"
+    }
+    //template Strings
+    println("SuperHeroe: ${superHeroe}")
+
+    when(casado){
+        true -> {
+            println("esta casado")
+        }
+        else -> {
+            println("No esta casado")
+        }
+    }
+
+
 }
