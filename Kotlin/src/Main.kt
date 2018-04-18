a1d8329afa3618382aca5721e62ee2ddc8ed7fd9
Marcelo Nieto <marcechelo25@hotmail.com>
2018-04-18 09:54:54 -0500

    Funcion y main

---
+++ Kotlin/src/Main.kt
@@ -0,0 +1,8 @@
+fun sumarDosNumeros(numeroUno: Int, numeroDos: Int): Int{
+    return numeroUno+numeroDos
+}
+
+fun main(args: Array<String>){
+    println("Hola Mundo") //system.out
+    println(sumarDosNumeros(1,2))
+}
